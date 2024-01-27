6059f2594ac7419836e989cbbac97c15643b385b
Maulik2797 <vasanimaulik372@gmail.com>
2024-01-27 11:29:45 +0530

    27-01-2024

---
+++ oops inheritance/single inheritance/new.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class A
+{
+    int a, b;
+
+public:
+    void getdata()
+    {
+        cout << "Enter value of a =" << endl;
+        cin >> a;
+        cout << "Enter value of b = " << endl;
+        cin >> b;
+
+        cout << "A = " << a << " "
+             << "B = " << b << endl;
+    }
+};
+class B : public A
+{
+    int m, n;
+
+public:
+    void setdata()
+    {
+        cout << "Enter value of M =" << endl;
+        cin >> m;
+        cout << "Enter value of N = " << endl;
+        cin >> n;
+
+        cout << "M = " << m << " "
+             << "N = " << n << endl;
+        
+        getdata();
+    }
+};
+/*class C: public A, public B
+{
+
+
+};*/
+int main()
+{
+    B b;
+    b.setdata();
+}
