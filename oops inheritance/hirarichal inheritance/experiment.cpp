70b8d5655d5a0a411e5f50ab3f6cbcd99283aa30
Maulik2797 <vasanimaulik372@gmail.com>
2024-01-27 13:47:00 +0530

    27-01-2024

---
+++ oops inheritance/hirarichal inheritance/experiment.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+class base
+{ protected:
+    int a, b, c;
+
+public:
+    void input()
+    {
+        cout << " Enter A and B " << endl;
+        cin >> a >> b;
+    }
+    /*void output()
+    {
+        cout << "Sum is = " << a + b << endl;
+    }*/
+};
+class derive1:public base
+{
+    int d;
+
+public:
+    /*void input1()
+    {
+        cout << " Enter d" << endl;
+        cin >> d;
+    }*/
+    void output1()
+    {
+        cout << "output is = " <<a*b<< endl;
+    }
+};
+class derive2: public base
+{
+    int e,f;
+
+public:
+    /*void input2()
+    {
+        cout << " Enter e " << endl;
+        cin >> e;
+    }*/
+    void output2()
+    {
+        cout << "output is = " <<a +b<< endl;
+    }
+};
+int main()
+{
+    /*base a;
+    a.input();
+    a.output();*/
+
+    derive1 d1;
+    d1.input();
+    //d1.output();
+
+    //d1.input1();
+    d1.output1();
+
+    derive2 d2;
+    d2.input();
+   // d2.output();
+
+    //d2.input2();
+    d2.output2();
+}
